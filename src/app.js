b9be16035bc7056f39449422d441e5ca1edfa29d
Ahmad Yaseen <itsahmadyaseen@gmail.com>
2024-03-06 11:14:03 +0530

    user routes and controllers

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
@@ -14,5 +15,11 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+//import routes
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/user", userRouter);
 
 export { app };
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,77 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { apiError } from "../utils/apiError.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { apiResponse } from "../utils/apiResponse.js";
+
+const registerUser = asyncHandler(async (req, res) => {
+  /*Steps
+        Get user details from frontend 
+        validation - not empty
+        check if user already exists: email or username
+        check for images
+        upload them to cloudinary
+        create user object -- create entry in DB
+        remove password and refresh token field from response
+        check for user creation
+        retur res   
+  */
+
+  const { fullName, username, email, password } = req.body;
+  console.log("Email ", email);
+
+  //all fields to be filled
+  if (
+    [fileName, username, email, password].some((field) => {
+      field?.trim === "";
+    })
+  ) {
+    throw new apiError(400, "All fields are required");
+  }
+
+  //check existing user
+  const existedUser = User.findOne({
+    $or: [{ email, username }],
+  });
+
+  if (existedUser) {
+    throw new apiError(409, "Username or Email already exists");
+  }
+
+  //get images
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new apiError(400, "Avatar file is required");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new apiError(400, "Avatar file is required");
+  }
+
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    username: username.toLowerCase(),
+    email,
+    password,
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new apiError(500, "Something went wrong while registering the user");
+  }
+
+  return res
+    .status(201)
+    .json(new apiResponse(200, createdUser, "User registered successfully"));
+});
+
+export { registerUser };
