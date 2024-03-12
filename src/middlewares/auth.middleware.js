c600511a06c8cf3cf3b792230eb355e61ae00693
Ahmad Yaseen <itsahmadyaseen@gmail.com>
2024-03-12 14:20:30 +0530

    .js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
-import { apiError } from "../utils/apiError";
-import { asyncHandler } from "../utils/asyncHandler";
-import { User } from "../models/user.model";
+import { apiError } from "../utils/apiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
 
 export const verifyJwt = asyncHandler(async (req, res, next) => {
   try {
