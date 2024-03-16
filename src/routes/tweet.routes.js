7f413ad4c6d8b673d25da4b2c628f26223e11c46
Ahmad Yaseen <itsahmadyaseen@gmail.com>
2024-03-16 23:25:37 +0530

    tweet route

---
+++ src/routes/tweet.routes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import {
+  createTweet,
+  deleteTweet,
+  getUserTweets,
+  updateTweet,
+} from "../controllers/tweet.controller";
+
+const router = Router();
+
+router.route("/create-tweet").post(createTweet);
+router.route("/user/:userId").get(getUserTweets);
+router.route("/:tweetId").patch(updateTweet).delete(deleteTweet);
+
+export default router;
